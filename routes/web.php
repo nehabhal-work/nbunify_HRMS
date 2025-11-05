46776456eeebebd2d98c1731fc7cc18b20a7087b
Tanmay Pradhan <tanmay@tasklabs.in>
2025-11-05 21:31:41 +0530

    Added route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Master\DepartmentController;
+use App\Http\Controllers\Master\DesignationController;
+use App\Http\Controllers\Master\SubDepartmentController;
+use App\Http\Controllers\Master\SubDesignationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Master\CompanyController;
 use App\Http\Controllers\Master\HeadOfficeController;
@@ -17,6 +21,10 @@ Route::middleware(['auth', 'verified'])->prefix('master')->name('master.')->grou
     Route::resource('companies', CompanyController::class);
     Route::resource('head-offices', HeadOfficeController::class);
     Route::resource('branches', BranchController::class);
+    Route::resource('departments', DepartmentController::class);
+    Route::resource('sub-departments', SubDepartmentController::class);
+    Route::resource('designations', DesignationController::class);
+    Route::resource('sub-designations', SubDesignationController::class);
 });
 
 require __DIR__ . '/settings.php';
