3715d949db6d21d81b4bc3d5675f822741a01ed6
Tanmay Pradhan <tanmay@tasklabs.in>
2025-11-07 21:40:49 +0530

    Deployment file

---
+++ config/client.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    'gender' => [
+        'male' => 'Male',
+        'female' => 'Female',
+        'other' => 'Other',
+    ],
+    'live_status' => [
+        'alive' => 'Alive',
+        'deceased' => 'Deceased',
+    ],
+    'martial_status' => [
+        'single' => 'Single',
+        'married' => 'Married',
+        'divorced' => 'Divorced',
+        'widowed' => 'Widowed',
+    ],
+    'nationality' => [
+        'ri' => 'Resident Individual',
+        'nro' => 'Non-Resident Ordinary',
+        'nre' => 'Non-Resident External',
+        'pio' => 'Person of Indian Origin',
+        'oci' => 'Overseas Citizen of India',
+        'gch' => 'Green Card Holder',
+        'trioc' => 'Tax Resident In Other Country',
+        'fn' => 'Foreign National',
+        'other' => 'Other',
+    ],
+    'occupation' => [
+        'private_sector' => 'Private Sector',
+        'public_sector' => 'Public Sector',
+        'government' => 'Government Sector',
+        'business' => 'Business',
+        'eduation' => 'Education Sector',
+        'professional' => 'Professional',
+        'agriculture' => 'Agriculture Sector',
+        'student' => 'Student',
+        'doctor' => 'Doctor',
+        'housewife' => 'Housewife',
+        'retired' => 'Retired',
+        'other' => 'Other',
+    ],
+];
