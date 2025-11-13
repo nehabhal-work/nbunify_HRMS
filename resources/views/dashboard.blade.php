3474dcb92acdb6dc2761d60a7b67eac7c0aa82f8
Bhalchandra <nehabhal.work@gmail.com>
2025-11-13 12:06:13 +0530

    testing version 3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,8 +42,9 @@
                                 <div class="card">
                                     <div class="card-body">
                                         <h4 class="card-title">Welcome to Dashboard by nehaa!!</h4>
-                                        <h2>again bhal</h2>
+                                        <h2>again bhal in process</h2>
                                         <h3>aata neha ragavali</h3>
+                                        <h5>new line insertion by bhal</h5>
                                         <p class="card-text">You are successfully logged in!</p>
                                         <a href="{{ route('master.companies.index') }}" class="btn btn-primary">Manage
                                             Companies</a>
