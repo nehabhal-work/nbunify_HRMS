f3ffa29d6342f5835f0b7b7abd5ca443926fd288
nehabhalwork <nehabhal.work@gmail.com>
2025-11-13 11:47:43 +0530

    testing els by neha chauhan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,7 +41,7 @@
                             <div class="col-12">
                                 <div class="card">
                                     <div class="card-body">
-                                        <h4 class="card-title">Welcome to Dashboard by nehaa!!</h4>
+                                        <h4 class="card-title">Welcome to Dashboard by nehaa from Els!!</h4>
                                         <p class="card-text">You are successfully logged in!</p>
                                         <a href="{{ route('master.companies.index') }}" class="btn btn-primary">Manage Companies</a>
                                     </div>
