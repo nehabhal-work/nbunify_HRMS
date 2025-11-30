b41fb47d0c9b68a8d460f2fad6108b1b3435f111
Bhalchandra <nehabhal.work@gmail.com>
2025-11-30 12:35:52 +0530

    investment create updated

--- resources/views/content/investment/create.blade.php
+++ resources/views/content/investment/create.blade.php
@@ -70,8 +70,26 @@
                             </div>
 
                             <!-- Investment Holder -->
-                            <div class="col-md-8 holderBox" id="holder_single">
-                                <label for="client_id" class="form-label">Investment Holder</label>
+                            <div class="col-md-3 holderBox" id="holder_single">
+                                <label for="client_id" class="form-label">Investment 1st Holder</label>
+                                <select class="form-select select2 @error('client_id') is-invalid @enderror"
+                                    name="client_id" id="client_id" required>
+                                    <option value="">Select Holder</option>
+                                    {{-- @foreach ($client as $d)
+                                        <option value="{{ $d->id }}">
+                                            {{ ucfirst(strtolower($d->fname)) }}
+                                        </option>
+                                        
+                                    @endforeach --}}
+                                </select>
+
+                                @error('client_id')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-5 holderBox" id="holder_single">
+                                <label for="client_id" class="form-label">Investment 2nd Holder</label>
                                 <select class="form-select select2 @error('client_id') is-invalid @enderror"
                                     name="client_id" id="client_id" required>
                                     <option value="">Select Holder</option>
@@ -204,32 +222,9 @@
                                 @enderror
                             </div>
 
-                            <!-- Maturity Date -->
-                            <div class="col-md-2">
-                                <label for="maturity_date" class="form-label">Maturity Date</label>
-                                <input type="date"
-                                    class="form-control bg-secondary-subtle @error('maturity_date') is-invalid @enderror"
-                                    name="maturity_date" id="maturity_date" readonly required>
 
-                                @error('maturity_date')
-                                    <small class="text-danger">{{ $message }}</small>
-                                @enderror
-                            </div>
 
-                            <!-- Payout Amount -->
-                            <div class="col-md-2">
-                                <label for="payout_amount" class="form-label">Interest / Payout Amount</label>
-                                <div class="input-group">
-                                    <span class="input-group-text">₹</span>
-                                    <input type="number"
-                                        class="form-control bg-secondary-subtle onlydigit @error('int_payout_amount') is-invalid @enderror"
-                                        name="int_payout_amount" id="payout_amount" readonly required>
-                                </div>
 
-                                @error('int_payout_amount')
-                                    <small class="text-danger">{{ $message }}</small>
-                                @enderror
-                            </div>
 
                             <!-- TDS -->
                             <div class="col-md-2">
@@ -245,40 +240,9 @@
                                 @enderror
                             </div>
 
-
-
-
-
-
-
-
                         </div>
 
-                        {{-- address section --}}
-                        <div class="row">
-                            <!-- Residential Address -->
 
-                            <!-- Company Bank (From) -->
-                            <div class="col-md-4">
-                                <label class="form-label text-danger">From Company Bank *</label>
-                                <select class="form-select" id="out_company_bank" name="out_company_bank" required>
-                                    <option value="">Select Company Bank</option>
-
-                                </select>
-
-                            </div>
-
-                            <!-- Client Bank (To) -->
-                            <div class="col-md-4">
-                                <label class="form-label text-danger">To Client Bank *</label>
-                                <select class="form-select to_client_bank" name="to_client_bank" required>
-                                    <option value="">Select Client Bank</option>
-                                </select>
-                            </div>
-
-
-
-                        </div>
 
                         <!-- Submit -->
                         <div class="text-end mt-3">
@@ -295,41 +259,6 @@
 
         </div>
     </form>
-
-
-
-    <div class="row">
-        <!-- TABLE SECTION -->
-
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h5 class="mb-0">investment List</h5>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive text-nowrap">
-                        <table class="table srkdataTable ">
-                            <thead class="table-light">
-                                <tr>
-                                    <th>#</th>
-                                    <th>investment</th>
-                                    <th>code</th>
-                                    <th>Contact Person</th>
-                                    <th>Number</th>
-                                    <th>Email</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {{-- Example static row (replace with @foreach later) --}}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
 
