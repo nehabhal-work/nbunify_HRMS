cba10c2a6f175203385ffd0e3a35b952e1042552
nehabhalwork <nehabhal.work@gmail.com>
2025-12-08 20:16:29 +0530

    web app solve

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
 Route::middleware(['auth', 'verified'])->prefix('master')->name('master.')->group(function () {
     Route::resource('companies', CompanyController::class);
     Route::resource('head-offices', HeadOfficeController::class);
