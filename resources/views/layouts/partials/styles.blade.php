da6e84333cf0d73003825582ebd7080cd2694cc4
Tanmay Pradhan <tanmay@tasklabs.in>
2025-12-22 17:12:28 +0530

    Error in IFSC code

--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,6 +1,9 @@
 <!-- Favicon -->
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.png') }}" />
 
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
