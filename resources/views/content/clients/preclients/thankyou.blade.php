5159a8570b510dadda0327e14cab39d22ec69bcf
Bhalchandra <nehabhal.work@gmail.com>
2026-01-15 18:52:34 +0530

    update thankyou page preclient

--- resources/views/content/clients/preclients/thankyou.blade.php
+++ resources/views/content/clients/preclients/thankyou.blade.php
@@ -3,13 +3,37 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Thank You</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body>
-    <h3>Thankyou for submit your information.</h3>
-</body>
+<body class="bg-light d-flex align-items-center justify-content-center vh-100">
 
-</html>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-7 col-lg-6">
+                <div class="card shadow-sm border-0">
+                    <div class="card-body text-center p-5">
+
+                        <h2 class="text-success mb-3">Thank You!</h2>
+
+                        <p class="fs-5 mb-4">
+                            Thank you for submitting your information.
+                            Our team will review the details and get back to you if any additional information is
+                            required.
+                        </p>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap 5 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
+        < /script
